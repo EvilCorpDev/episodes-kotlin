96a2393be5774fbbd29290ce200978492e0d8965
Zakhar Kliap <Zakhar_Kliap@epam.com>
2016-07-12 11:42:25 +0300

    add config

--- src/main/kotlin/com/nomaddev/config/ServerRootConfig.kt
+++ src/main/kotlin/com/nomaddev/config/ServerRootConfig.kt
@@ -1,7 +1,12 @@
 package com.nomaddev.config
 
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.Configuration
+
 /**
  * Created by Zakhar_Kliap on 07-Jul-16.
  */
+@Configuration
+@ComponentScan("com.mkyong.helloworld.service")
 class ServerRootConfig {
 }
