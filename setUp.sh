bd88f0ad71254663667ec7cc62675c566850d106
Zakhar Kliap <zakhar_kliap@epam.com>
2017-02-22 15:04:24 +0200

    change setUp

--- setUp.sh
+++ setUp.sh
@@ -1,8 +1,6 @@
 sudo add-apt-repository ppa:webupd8team/java
 sudo apt-get update
 sudo apt-get install oracle-java8-installer
-sudo apt-get update
-sudo apt-get install git
 sudo add-apt-repository ppa:cwchien/gradle
 sudo apt-get update
 sudo apt-get install gradle
