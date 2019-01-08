830da77b323059365fd408375692657a8da4acf7
Zakhar Kliap <zkliap@mirantis.com>
2019-01-08 19:17:46 +0200

    remove gretty

--- src/main/kotlin/com/nomaddev/server/services/entity/EpisodeUserDetails.kt
+++
@@ -1,8 +0,0 @@
-package com.nomaddev.server.services.entity
-
-import org.springframework.security.core.GrantedAuthority
-import org.springframework.security.core.userdetails.User
-
-class EpisodeUserDetails
-constructor(username: String, password: String, authorities: MutableCollection<out GrantedAuthority?>):
-        User(username, password, authorities)
